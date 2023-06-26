e23a1e4c7a685931ff050843767bdf302e535f31
matth <mattheew.oliveira@gmail.com>
2023-06-26 10:22:32 -0300

    Enviando projeto

---
+++ src/main/java/br/com/alura/screenmatch/domain/filme/Filme.java
@@ -0,0 +1,64 @@
+package br.com.alura.screenmatch.domain.filme;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name="filmes")
+public class Filme {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+    private Integer duracaoEmMinutos;
+    private Integer anoLancamento;
+    private String genero;
+
+    public Filme(DadosCadastroFilme dados) {
+        this.nome = dados.nome();
+        this.duracaoEmMinutos = dados.duracao();
+        this.anoLancamento = dados.ano();
+        this.genero = dados.genero();
+    }
+
+    public Filme() {
+
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public Integer getDuracaoEmMinutos() {
+        return duracaoEmMinutos;
+    }
+
+    public Integer getAnoLancamento() {
+        return anoLancamento;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    @Override
+    public String toString() {
+        return "Filme{" +
+                "nome='" + nome + '\'' +
+                ", duracaoEmMinutos=" + duracaoEmMinutos +
+                ", anoLancamento=" + anoLancamento +
+                ", genero='" + genero + '\'' +
+                '}';
+    }
+
+    public void atualizaDados(DadosAlteracaoFilme dados) {
+        this.nome = dados.nome();
+        this.duracaoEmMinutos = dados.duracao();
+        this.anoLancamento  = dados.ano();
+        this.genero = dados.genero();
+    }
+}
